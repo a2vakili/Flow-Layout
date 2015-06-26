29d5262dafedc3807c76b30cbdf42d32bab70909
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-26 15:03:50 -0400

    Initial Commit

---
+++ Cover-Flow-Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cover-Flow-Layout
+//
+//  Created by Arsalan Vakili on 2015-06-26.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Cover-Flow-LayoutTests/Cover_Flow_LayoutTests.m
@@ -0,0 +1,40 @@
+//
+//  Cover_Flow_LayoutTests.m
+//  Cover-Flow-LayoutTests
+//
+//  Created by Arsalan Vakili on 2015-06-26.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Cover_Flow_LayoutTests : XCTestCase
+
+@end
+
+@implementation Cover_Flow_LayoutTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
